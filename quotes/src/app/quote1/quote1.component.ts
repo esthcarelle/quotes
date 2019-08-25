5a7ef3059966ac5dd77520ea8ddda5517b1c827a
esthcarelle <esthcarelle@gmail.com>
2019-08-25 14:55:18 +0200

    add welcome page

--- quotes/src/app/quote1/quote1.component.ts
+++ quotes/src/app/quote1/quote1.component.ts
@@ -8,10 +8,10 @@ import { Quote} from '../quote';
 export class Quote1Component implements OnInit {
  quotes:Quote[]=[
   new Quote("Wil","I have a dream that one day little black boys and girls will be holding hands with little white boys and girls","Martin Luther King",new Date(2019,8,23)),
-  new Quote("Merlyn","Be kind whenever possible. It is always possible","Dalai Lama",new Date(2019,8,24)),
-  new Quote("Raissa","Life is a flower of which love is the honey","Victor hugo",new Date(2019,8,24)),
-   new Quote("Lyse","Whatever you are, be a good one","Abraham Lincoln",new Date(2019,8,24)),
-   new Quote("steve","wA great man is always willing to be little","Ralph WAldo",new Date(2019,8,25))
+  new Quote("Merlyn","Be kind whenever possible. It is always possible","Dalai Lama",new Date(2019,7,24)),
+  new Quote("Raissa","Life is a flower of which love is the honey","Victor hugo",new Date(2019,7,24)),
+   new Quote("Lyse","Whatever you are, be a good one","Abraham Lincoln",new Date(2019,7,24)),
+   new Quote("steve","wA great man is always willing to be little","Ralph WAldo",new Date(2019,7,25))
  ]
  showDetails(index){
   this.quotes[index].showDescription = !this.quotes[index].showDescription;
