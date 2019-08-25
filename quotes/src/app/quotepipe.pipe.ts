83d60163d2eb19e4d00b183f7c49f5ed735008a2
esthcarelle <esthcarelle@gmail.com>
2019-08-25 13:56:36 +0200

    add time passed to th quote

--- quotes/src/app/quotepipe.pipe.ts
+++ quotes/src/app/quotepipe.pipe.ts
@@ -7,14 +7,14 @@ export class QuotepipePipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
     let thisday:Date = new Date(); //get current date and time
-    let thisdayWithNoTime:any = new Date(thisday.getHours(), thisday.getMinutes(), thisday.getMilliseconds())
-    let dayWithNoTime:any = new Date(value.getHours(), value.getMinutes(), value.getMilliseconds())
+    let thisdayWithNoTime:any = new Date(thisday.getFullYear(), thisday.getMonth(), thisday.getDate())
+    let dayWithNoTime:any = new Date(value.getFullYear(), value.getMonth(), value.getDate())
     var dateDifference = Math.abs(dayWithNoTime - thisdayWithNoTime) //returns value in miliseconds
-    const secondsInDay = 3600; //60 seconds * 60 minutes in an hour * 24 hours in a day
+    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
     var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
     var dateCounter = dateDifferenceSeconds/secondsInDay;
 
-    if (dateCounter >= 1 && value >= thisdayWithNoTime){
+    if (dateCounter >= 1 && value >thisdayWithNoTime){
       return dateCounter ;
     }else{
       return 0;
