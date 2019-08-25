6c8107ce4a4eb3515282c0bc2b1e6c5b1e8bbeaa
esthcarelle <esthcarelle@gmail.com>
2019-08-25 14:29:41 +0200

    adding some styles to quotes

--- quotes/src/app/quote1/quote1.component.ts
+++ quotes/src/app/quote1/quote1.component.ts
@@ -7,9 +7,11 @@ import { Quote} from '../quote';
 })
 export class Quote1Component implements OnInit {
  quotes:Quote[]=[
-
-   new Quote("Abraham Lincoln","Whatever you are, be a good one","lyse",new Date(2019,8,24)),
-   new Quote("Ralph WAldo","wA great man is always willing to be little","steve",new Date(2019,8,25))
+  new Quote("Wil","I have a dream that one day little black boys and girls will be holding hands with little white boys and girls","Martin Luther King",new Date(2019,8,23)),
+  new Quote("Merlyn","Be kind whenever possible. It is always possible","Dalai Lama",new Date(2019,8,24)),
+  new Quote("Raissa","Life is a flower of which love is the honey","Victor hugo",new Date(2019,8,24)),
+   new Quote("Lyse","Whatever you are, be a good one","Abraham Lincoln",new Date(2019,8,24)),
+   new Quote("steve","wA great man is always willing to be little","Ralph WAldo",new Date(2019,8,25))
  ]
  showDetails(index){
   this.quotes[index].showDescription = !this.quotes[index].showDescription;
