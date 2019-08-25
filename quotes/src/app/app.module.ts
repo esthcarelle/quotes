4b4327a6470728aecf6530aefd8485b6bc58254b
esthcarelle <esthcarelle@gmail.com>
2019-08-25 23:57:15 +0200

    change pipe

--- quotes/src/app/app.module.ts
+++ quotes/src/app/app.module.ts
@@ -7,18 +7,18 @@ import { Quote1Component } from './quote1/quote1.component';
 import { FormsModule } from '@angular/forms';
 import { QuotedescriptionComponent } from './quotedescription/quotedescription.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { QuotepipePipe } from './quotepipe.pipe';
 import { VoteComponent } from './vote/vote.component';
 import { HighlightDirective } from './highlight.directive';
+import {TimeAgoPipe} from 'time-ago-pipe';
 @NgModule({
   declarations: [
     AppComponent,
     Quote1Component,
     QuotedescriptionComponent,
     QuoteFormComponent,
-    QuotepipePipe,
     VoteComponent,
-    HighlightDirective
+    HighlightDirective,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
--- quotes/src/app/quotepipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { QuotepipePipe } from './quotepipe.pipe';
-
-describe('QuotepipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new QuotepipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
